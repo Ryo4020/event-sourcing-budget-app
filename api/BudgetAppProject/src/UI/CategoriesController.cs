35d7ea382eb5a1f7f293530dd8c713e055f5ef2b
Ryo4020 <gryo.1440@gmail.com>
2025-06-16 17:47:28 +0900

    add Controllers in UI layer

---
+++ api/BudgetAppProject/src/UI/CategoriesController.cs
@@ -0,0 +1,64 @@
+namespace BudgetAppProject.UI;
+
+using Microsoft.AspNetCore.Mvc;
+using BudgetAppProject.Application.Usecase.DeleteCategory;
+using BudgetAppProject.Application.Usecase.GetCategories;
+using BudgetAppProject.Application.Usecase.RegisterCategory;
+using BudgetAppProject.Application.Usecase.RenameCategory;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriesController : ControllerBase
+{
+    private readonly IRegisterCategoryUsecase _registerCategoryUsecase;
+    private readonly IRenameCategoryUsecase _renameCategoryUsecase;
+    private readonly IDeleteCategoryUsecase _deleteCategoryUsecase;
+    private readonly IGetCategoriesUsecase _getCategoriesUsecase;
+
+    public CategoriesController (
+        IRegisterCategoryUsecase registerCategoryUsecase,
+        IRenameCategoryUsecase renameCategoryUsecase,
+        IDeleteCategoryUsecase deleteCategoryUsecase,
+        IGetCategoriesUsecase getCategoriesUsecase
+    )
+    {
+        _registerCategoryUsecase = registerCategoryUsecase;
+        _renameCategoryUsecase = renameCategoryUsecase;
+        _deleteCategoryUsecase = deleteCategoryUsecase;
+        _getCategoriesUsecase = getCategoriesUsecase;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register([FromBody] RegisterCategoryRequest request)
+    {
+        await _registerCategoryUsecase.HandleAsync(request);
+
+        return Created();
+    }
+
+    [HttpPut("rename")]
+    public async Task<IActionResult> Rename([FromBody] RenameCategoryRequest request)
+    {
+        await _renameCategoryUsecase.HandleAsync(request);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete([FromRoute] string id)
+    {
+        var request = new DeleteCategoryRequest() { Id = id };
+        await _deleteCategoryUsecase.HandleAsync(request);
+
+        return NoContent();
+    }
+
+    [HttpGet("{userId}")]
+    public async Task<IActionResult> GetAll([FromRoute] string userId)
+    {
+        var request = new GetCategoriesRequest() { UserId = userId };
+        var categories = await _getCategoriesUsecase.HandleAsync(request);
+
+        return Ok(categories);
+    }
+}
