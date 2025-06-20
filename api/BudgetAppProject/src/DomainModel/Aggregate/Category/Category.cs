e11c65aa15f41954d556261de3fd371718d3ebbc
Ryo4020 <gryo.1440@gmail.com>
2025-06-21 00:20:12 +0900

    add Category Entity

---
+++ api/BudgetAppProject/src/DomainModel/Aggregate/Category/Category.cs
@@ -0,0 +1,48 @@
+namespace BudgetAppProject.DomainModel.Aggregate.MoneyOperation;
+
+using BudgetAppProject.DomainModel.Utils;
+
+public class Category
+{
+    public string Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public bool IsDefault { get; private set; }
+
+    public string UserId { get; private set; }
+
+    public Category(
+        string id,
+        string name,
+        bool isDefault,
+        string userId
+    )
+    {
+        Id = id;
+        Name = name;
+        IsDefault = isDefault;
+        UserId = userId;
+    }
+
+    public static Category Create(
+        string name,
+        string userId
+    )
+    {
+        var id = UuidGenerator.NewUuid();
+        return new Category(
+            id,
+            name,
+            false,
+            userId
+        );
+    }
+
+    public void Rename(
+        string name
+    )
+    {
+        Name = name;
+    }
+}
