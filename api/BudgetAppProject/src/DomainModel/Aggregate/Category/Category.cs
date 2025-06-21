9d3d699863abed4344a07b213cffa6c8e7712002
Ryo4020 <gryo.1440@gmail.com>
2025-06-21 17:18:13 +0900

    fix Category AggregateRoot

--- api/BudgetAppProject/src/DomainModel/Aggregate/Category/Category.cs
+++ api/BudgetAppProject/src/DomainModel/Aggregate/Category/Category.cs
@@ -1,8 +1,6 @@
 namespace BudgetAppProject.DomainModel.Aggregate.Category;
 
-using BudgetAppProject.DomainModel.Utils;
-
-public class Category
+public abstract class Category
 {
     public string Id { get; init; }
 
@@ -10,40 +8,27 @@ public class Category
 
     public bool IsDefault { get; init; }
 
-    public string UserId { get; init; }
-
     public Category(
         string id,
         string name,
-        bool isDefault,
-        string userId
+        bool isDefault
     )
     {
+        if (!isDefault && name == DefaultCategory.NameReplaceDeleted)
+        {
+            throw new ArgumentException("Cannot create a category with the default name.");
+        }
+
         Id = id;
         Name = name;
         IsDefault = isDefault;
-        UserId = userId;
     }
 
-    public static Category Create(
-        string name,
-        string userId
-    )
+    protected void SetName(string name)
     {
-        var id = UuidGenerator.NewUuid();
-        return new Category(
-            id,
-            name,
-            false,
-            userId
-        );
-    }
-
-    public void Rename(string name)
-    {
-        if (IsDefault)
+        if (name == DefaultCategory.NameReplaceDeleted)
         {
-            throw new InvalidOperationException("Cannot rename a default category.");
+            throw new ArgumentException("Cannot rename a category to the default name.");
         }
 
         Name = name;
