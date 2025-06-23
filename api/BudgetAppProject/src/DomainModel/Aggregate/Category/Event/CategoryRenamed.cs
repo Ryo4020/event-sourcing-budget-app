816b4804841123ac5ed26fb5e94bac4b13e62482
Ryo4020 <gryo.1440@gmail.com>
2025-06-23 21:24:20 +0900

    fix name of CategoryRenamed event

--- api/BudgetAppProject/src/DomainModel/Aggregate/Category/Event/CategoryRenamed.cs
+++ api/BudgetAppProject/src/DomainModel/Aggregate/Category/Event/CategoryRenamed.cs
@@ -2,13 +2,13 @@ namespace BudgetAppProject.DomainModel.Aggregate.Category.Event;
 
 using BudgetAppProject.DomainModel.SeedWork;
 
-public class CategoryEdited : DomainEvent
+public class CategoryRenamed : DomainEvent
 {
     public string EventTargetid { get; init; }
     
     public string NewName { get; init; }
 
-    public CategoryEdited(string categoryId, string newName) : base()
+    public CategoryRenamed(string categoryId, string newName) : base()
     {
         EventTargetid = categoryId;
         NewName = newName;
